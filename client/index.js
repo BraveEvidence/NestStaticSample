7de8d4453453c0a2fb5a983492acea9a1cf6bf0a
Pritish <pritish@transformhub.com>
2024-11-29 13:22:54 +0530

    first commit

---
+++ client/index.js
@@ -0,0 +1,5 @@
+//get hold of the button by its ids and add a click listener to it
+document.getElementById('mybutton').addEventListener('click', () => {
+  //get hold of the link by its ids
+  alert('Hello');
+});
